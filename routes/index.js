50d9f255f06fad37833cba1e4dafd1a6a46cf416
N. Tanner Smith <smith.tanner2222@gmail.com>
2022-04-20 09:59:10 -0600

    added assignment details

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const routes = require('express').Router();
 const jokes = require('../controllers/');
 
-routes.get('/', jokes.displayJoke); // gets the path (home) and grabs the function from the controller
+// routes.get('/', jokes.displayJoke); // gets the path (home) and grabs the function from the controller
+
+routes.get('/', (req, res) => {
+    res.send('Riley Skinner');
+});
 
 module.exports = routes;
